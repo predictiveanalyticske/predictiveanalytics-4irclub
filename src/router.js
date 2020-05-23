02d14ff599ab2e442d9a3ecc192d74cdb7deb089
Bralco Creations <info@bralcocreations.co.ke>
2020-05-23 19:03:51 +0300

    Router Update 0.0.4

--- src/router.js
+++ src/router.js
@@ -11,33 +11,57 @@ import Home from './components/global/HomeComponent.vue';
 import HomeIndex from './components/home/layouts/LandingComponent.vue';
 import HomeDocumentaries from './components/home/layouts/DocumentariesComponent.vue';
 import HomePlans from './components/home/layouts/PlansComponent.vue';
+import HomePlan from './components/home/layouts/PlanComponent.vue';
+import HomeAuth from './components/home/layouts/AuthComponent.vue';
 
 Vue.use(Router);
 
 export default new Router({
    routes: [
-        {
+            {
             path: '/',
             children:[
-            {
-                path: '',
-                components: {
-                    view: HomeIndex,
-                }
-            },
-            {
-                path: '/documentaries',
-                components: {
-                    view: HomeDocumentaries,
-                }
-            },
-            {
-                name: "plans",
-                path: '/plans',
-                components: {
-                    view: HomePlans,
-                }
-            },
+                    {
+                        path: '',
+                        components: {
+                            view: HomeIndex,
+                        }
+                    },
+                    {
+                        name: "documentaries",
+                        path: '/documentaries',
+                        components: {
+                            view: HomeDocumentaries,
+                        },
+                        beforeEnter (to,from, next) {
+                            if( this.$store.state.app.auth.isAuthenticated ){
+                                next();
+                            } else {
+                                next({name: "auth"});
+                            }
+                        }
+                    },
+                    {
+                        name: "plans",
+                        path: '/plans',
+                        children: [
+                            {
+                                name: "plan",
+                                path: ':item',
+                                component: HomePlan
+                            },
+                        ],
+                        components: {
+                            view: HomePlans,
+                        }
+                    },
+                    {
+                        name: "auth",
+                        path: '/login',
+                        components: {
+                            view: HomeAuth,
+                        }
+                    },
             ],
             components: {
                 default: App,
