19daa4b7a647fad482af42ac8dc823b61fa0798b
Bralco Creations <info@bralcocreations.co.ke>
2020-05-26 12:58:59 +0300

    Router Update 0.1.12

--- src/router.js
+++ src/router.js
@@ -22,6 +22,7 @@ export default new Router({
             path: '/',
             children:[
                     {
+                        name:"home",
                         path: '',
                         components: {
                             view: HomeIndex,
