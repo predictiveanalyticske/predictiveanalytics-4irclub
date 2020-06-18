2ee84d8a0ccdd5bbeabedd8476e51c110a667705
Bralco Creations <info@bralcocreations.co.ke>
2020-06-18 16:19:02 +0300

    Profile UI Update 0.6.50

--- src/components/home/layouts/ProfileComponent.vue
+++ src/components/home/layouts/ProfileComponent.vue
@@ -2,7 +2,7 @@
     <vk-grid class="uk-child-width-1-1 uk-margin-remove">
         <div class="uk-padding-remove uk-margin-remove">
           <vk-card padding="large">
-            <vk-tabs align="center" animation="fade" class="uk-padding-large">
+            <vk-tabs align="center" animation="fade" class="uk-padding-small">
                 <vk-tabs-item title="Profile">
                     <div class="uk-flex uk-flex-center">
                         <div class="uk-width-1-2">
@@ -46,8 +46,8 @@
                                             <vk-button htmlType="submit" :class="'uk-button uk-button-primary'"> Save Changes </vk-button>
                                         </div>
                                     </div>
-                                    <div class="uk-width-1-2 uk-padding-large" v-else>
-                                        <vk-spinner ratio="3"></vk-spinner>
+                                    <div class="uk-width-1-2 uk-padding-large uk-flex-center" v-else>
+                                        <vk-spinner ratio="5"></vk-spinner>
                                     </div>
                                 </fieldset>
                             </form>
@@ -67,32 +67,41 @@
                         </div>
                     </div>
                 </vk-tabs-item>
+                <vk-tabs-item title="Payments">
+                    <div class="uk-flex uk-flex-center">
+                        
+                    </div>
+                </vk-tabs-item>
                 <vk-tabs-item title="Security">
-                        <form action="" method="POST" @click.prevent="submitForm">
-                            <fieldset class="uk-fieldset">
-                                <h2 class="uk-legend">Account Details</h2><hr>
-                                <div class="uk-width-1-2" >
-                                    <div class="uk-margin">
-                                        <label>Current Password</label>
-                                        <input class="uk-input" type="text" name="currentpassword" placeholder="First Name">
-                                    </div>
+                    <div class="uk-flex uk-flex-center">
+                        <div class="uk-width-1-2">
+                            <form action="" method="POST" @click.prevent="submitForm">
+                                <fieldset class="uk-fieldset">
+                                    <h2 class="uk-legend">Account Security</h2><hr>
+                                    <div class="uk-width-1-1" >
+                                        <div class="uk-margin">
+                                            <label>Current Password</label>
+                                            <input class="uk-input" type="text" name="currentpassword" placeholder="First Name">
+                                        </div>
 
-                                    <div class="uk-margin">
-                                        <label>New Password</label>
-                                        <input class="uk-input" type="text" name="newpassword" placeholder="New Password">
-                                    </div>
+                                        <div class="uk-margin">
+                                            <label>New Password</label>
+                                            <input class="uk-input" type="text" name="newpassword" placeholder="New Password">
+                                        </div>
 
-                                    <div class="uk-margin">
-                                        <label>Confirm New Password</label>
-                                        <input class="uk-input" type="text" name="newpassword_confirmation" placeholder="Confirm New Password">
-                                    </div>
+                                        <div class="uk-margin">
+                                            <label>Confirm New Password</label>
+                                            <input class="uk-input" type="text" name="newpassword_confirmation" placeholder="Confirm New Password">
+                                        </div>
 
-                                    <div class="uk-margin">
-                                        <vk-button htmlType="submit" :class="'uk-button uk-button-primary'"> Update </vk-button>
+                                        <div class="uk-margin">
+                                            <vk-button htmlType="submit" :class="'uk-button uk-button-primary'"> Update </vk-button>
+                                        </div>
                                     </div>
-                                </div>
-                            </fieldset>
-                        </form>                  
+                                </fieldset>
+                            </form>                 
+                        </div> 
+                    </div> 
                 </vk-tabs-item>
             </vk-tabs>
           </vk-card>
@@ -115,7 +124,7 @@ export default {
                     return [{
                         active: false,
                         title:  this.fields.user.subscription.subscription.category,
-                        html:   '<div class="uk-padding-small"><h3>Features</h3><hr><h4>'+this.fields.user.subscription.subscription.name+'</h4><h$<p class="uk-margin-remove">'+this.fields.user.subscription.subscription.details+'</p></div>'
+                        html:   '<div class="uk-padding-small"><h3>Features</h3><hr><h4>'+this.fields.user.subscription.subscription.details+'</h4><hr><</div>'
                     }];
                 } else {
                     return [];
@@ -141,6 +150,12 @@ export default {
                 this.bralcoaxios({ url: this.$store.state.app.env.backend_url + '/api/v1/4irclub/profile', request: 'GET' }).then( (response) => {
                     let resolve = this.bralcoresponse(response);
                     this.fields.user = resolve.data;
+                    this.fields.user.subscription.subscription.details = '<ol>';
+                    let self = this;
+                    JSON.parse(this.fields.user.subscription.subscription.features).forEach( function(item){
+                        self.fields.user.subscription.subscription.details += '<li>'+item+'</li>';
+                    });
+                    this.fields.user.subscription.subscription.details += '</ol>';
                 });
                 this.$store.commit('banner_title','Profile');
                 this.$store.commit('banner_content','');
