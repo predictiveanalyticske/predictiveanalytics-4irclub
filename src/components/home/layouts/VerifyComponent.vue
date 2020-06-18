71b467b2721aba3fc39a5316d468d00fe61bba82
Bralco Creations <info@bralcocreations.co.ke>
2020-06-18 13:49:13 +0300

    Verify UI Update 0.6.43

--- src/components/home/layouts/VerifyComponent.vue
+++ src/components/home/layouts/VerifyComponent.vue
@@ -2,14 +2,18 @@
     <vk-grid class="uk-child-width-1-1 uk-margin-remove">
         <div class="uk-padding-remove uk-margin-remove">
           <vk-card padding="large">
-            <div class="success-checkmark">
-              <div class="check-icon">
-                <span class="icon-line line-tip"></span>
-                <span class="icon-line line-long"></span>
-                <div class="icon-circle"></div>
-                <div class="icon-fix"></div>
+              <div class="uk-text-center">
+                  <div class="success-checkmark">
+                    <div class="check-icon">
+                      <span class="icon-line line-tip"></span>
+                      <span class="icon-line line-long"></span>
+                      <div class="icon-circle"></div>
+                      <div class="icon-fix"></div>
+                    </div>
+                  </div>
+                  <h1 class="br-heading">Your email address is verified.</h1>
+                  <h2>You will be redirected to login in {{ count.value }} seconds</h2>
               </div>
-            </div>
           </vk-card>
         </div>
     </vk-grid>
@@ -21,15 +25,48 @@
         data () {
           return {
             loading: true,
-            verified: false
+            verified: false,
+            count: {
+              type: Number,
+              value: 5
+            }
           }
         },
-        created () {
-          // this.bralcoaxios({ url: this.$store.state.app.env.backend_url+'/api/v1/4irclub/auth/email/verify' , request: 'POST', form: { '_method': 'PUT', 'token': this.$route.params.token } }).then( (response) => {
-          //     let resolve   = this.bralcoresponse(response);
-          //     this.loading  = false;
-          //     this.verified = resolve.verified;
-          // });
+        beforeMount(){
+             this.$store.commit('banner_title','Verify Emaill Address')
+             this.$store.commit('banner_content','');
+        },
+        beforeRouteEnter (to,from,next) {
+          next( vm => {
+            vm.initData(),
+            next()
+          });
+        },
+        mounted (){
+          this.countDownTimer();
+        },
+        methods:{
+          countDownTimer() {
+            let self = this;
+            if(this.count.value > 0) {
+                setTimeout(function() {
+                    self.count.value -= 1;
+                    self.countDownTimer()
+                }, 1000)
+            } else {
+              this.$router.push({name:'auth'});
+            }
+          },
+          initData(){
+            this.bralcoaxios({ url: this.$store.state.app.env.backend_url+'/api/v1/4irclub/auth/email/verify' , request: 'POST', form: { '_method': 'PUT', 'token': this.$route.params.token } }).then( (response) => {
+                let resolve   = this.bralcoresponse(response);
+                this.loading  = false;
+                this.verified = resolve.verified;
+            });
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+            this.$store.commit('loader',false);
+          }
         }
     }
 </script>
