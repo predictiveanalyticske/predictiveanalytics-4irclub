a3d276752900d0f82a62fe1faf5b95628f383f60
Bralco Creations <info@bralcocreations.co.ke>
2020-06-18 13:44:36 +0300

    Footer UI Update 0.6.37

--- src/components/home/header/FooterComponent.vue
+++ src/components/home/header/FooterComponent.vue
@@ -1,7 +1,44 @@
 <template>
-    <div class="uk-width-1-1">
-        <vk-card :class="'br-footer'">
-            <p>&copy;{{ year }} All rights reserved | {{ data.company }}</p>
+    <div class="uk-width-1-1 br-footer uk-light">
+        <vk-card type="blank">
+            <vk-grid class="uk-child-width-1-1">
+                <div>
+                    <vk-grid class="uk-child-width-1-2@xl uk-child-width-1-2@l uk-child-width-1-2@m uk-child-width-1-1@s uk-padding-small">
+                        <div class="uk-position-relative uk-padding-large">
+                            <img src="@/assets/images/logo-full-white.png" class="uk-position-center"/>
+                        </div>
+                        <div class="uk-light">
+                            <h3>Contact Us</h3>
+                            <form @submit.prevent="attemptLogin" :action="$store.getters.backendurl+'/api/v1/auth/login'" method="POST">
+                                <fieldset class="uk-fieldset">
+
+                                    <div class="uk-margin uk-light">
+                                        <label>Message</label>
+                                        <textarea class="uk-textarea" rows="5" name="message"></textarea>
+                                    </div>
+                                    <div class="uk-margin">
+                                        <label>Email</label>
+                                        <input class="uk-input" name="email" type="email" placeholder="Email Address">
+                                    </div>
+
+                                    <div class="uk-margin uk-light">
+                                        <vk-button htmlType="submit" size="large">Send</vk-button>
+                                    </div>
+
+                                </fieldset>
+                            </form>
+                        </div>
+                    </vk-grid><hr>
+                </div>
+                <div class="uk-text-center">
+                    <vk-iconnav class="uk-flex-center">
+                        <vk-iconnav-item href="https://predictiveanalytics.co.ke" target="blank" icon="world"></vk-iconnav-item>
+                        <vk-iconnav-item href="https://www.facebook.com" target="blank" icon="facebook"></vk-iconnav-item>
+                        <vk-iconnav-item href="https://predictiveanalytics.co.ke" target="blank" icon="linkedin"></vk-iconnav-item>
+                    </vk-iconnav>
+                    <p>&copy;{{ year }} All rights reserved | Predictive Analytics</p>
+                </div>
+            </vk-grid>
         </vk-card>
     </div>
 </template>
@@ -18,11 +55,3 @@
         }
     }
 </script>
-
-<style>
-    .br-footer {
-        background-color: #1d1d1d !important;
-        color: #fff !important;
-    }
-
-</style>
