3f8ee6c0ac7d0572f4c2157e94e8f6fd92871e6a
Bralco Creations <info@bralcocreations.co.ke>
2020-06-19 11:06:06 +0300

    Package Json Update 0.6.56

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import BralcoHelpers from './bralcohelpers'
 import router from './router'
 import store from './stores'
-
+import VuePhoneNumberInput from 'vue-phone-number-input';
+import 'vue-phone-number-input/dist/vue-phone-number-input.css';
+ 
 require('./croppie');
 require('./vuikit');
 
 Vue.use(BralcoHelpers);
-
+Vue.component('vue-phone-number-input', VuePhoneNumberInput);
 Vue.config.productionTip = false
 
 new Vue({
