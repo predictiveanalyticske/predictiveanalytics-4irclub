8689c0f59cebd1cf085972c886102ff049874efb
Bralco Creations <info@bralcocreations.co.ke>
2020-06-23 15:09:25 +0300

    Navabar Component Update 0.6.62

--- src/components/home/header/NavbarComponent.vue
+++ src/components/home/header/NavbarComponent.vue
@@ -8,7 +8,7 @@
     <vk-navbar transparent class="uk-hidden@xl uk-hidden@l uk-hidden@m">
         <vk-navbar-toggle title="Menu" @click="$store.commit('sidebar',true)"></vk-navbar-toggle>
         <vk-navbar-logo slot="center">
-            <img src="@/assets/images/logo-full-white.png" width="50%"/>
+            <img src="@/assets/images/logo-full-white.png" width="90%"/>
         </vk-navbar-logo>
     </vk-navbar>
     <vk-navbar transparent class="br-navbar uk-visible@s uk-visible@m">
@@ -25,20 +25,42 @@
             <vk-nav-item href="#" title="Logout" @click.prevent="logout" v-if="$store.getters.isAuthenticated"></vk-nav-item>
         </vk-navbar-nav>
     </vk-navbar>
-    <div class="uk-width-1-1 uk-inline" id="banner">
+    <div class="uk-width-1-1 uk-inline uk-visible@s uk-visible@m uk-visible@l" id="banner">
       <img src="@/assets/images/banner-full.png" v-if="banner"/>
-      <img src="@/assets/images/banner-small.png" v-else/>
-      <div class="uk-position-cover uk-overlay uk-overlay-default uk-flex uk-text-center uk-flex-center uk-light uk-flex-middle uk-padding-large">
-        <div class="uk-width-2-3">
+      <img src="@/assets/images/banner-small.png" v-if="!banner"/>
+      <div class="uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-light uk-flex-middle uk-padding-large">
+        <div class="uk-width-2-3 uk-visible@s uk-visible@m uk-text-center">
            <h1 class="uk-text-bolder br-banner-title uk-visible@s uk-visible@m">{{ title }}</h1>
-           <h4 class="uk-hidden@xl uk-hidden@l uk-hidden@m uk-margin-remove">{{ title }}</h4>
            <h2 class="uk-heading-large br-banner-sub_title uk-visible@s uk-visible@m">{{ message }}</h2>
-           <h5 class="uk-hidden@xl uk-hidden@l uk-hidden@m uk-margin-remove">{{ message }}</h5>
            <a v-if="banner" :href="$router.resolve({name:'about'}).href" class="uk-button uk-visible@s uk-visible@m uk-button-default uk-button-large">Read More</a>
-           <a v-if="banner" :href="$router.resolve({name:'about'}).href" class="uk-margin uk-button uk-hidden@xl uk-hidden@l uk-hidden@m uk-button-default uk-button-small">Read More</a>
        </div>
       </div>
     </div>
+    <div class="uk-width-1-1 uk-inline uk-hidden@xl uk-hidden@l uk-hidden@m" id="banner">
+      <img src="@/assets/images/banner-full-small.png" class="uk-hidden@xl uk-hidden@l uk-hidden@m" v-if="banner"/>
+      <img src="@/assets/images/banner-small-mini.png" v-else/>
+      <div class="uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-light uk-flex-middle uk-padding-large">
+        <div class="uk-width-1-1">
+           <h1 class="br-heading">{{ title }}</h1>
+           <h3 class="uk-margin-remove">{{ message }}</h3>
+           <a v-if="banner" :href="$router.resolve({name:'about'}).href" class="uk-margin uk-button uk-button-default uk-button-large">Read More</a>
+       </div>
+      </div>
+    </div>
+    <vk-modal-full :show.sync="show" class="br-modal-fixed">
+      <vk-modal-full-close large @click="$store.commit('sidebar',false)"></vk-modal-full-close>
+        <div>
+        <vk-nav class="uk-margin-vertical uk-padding-large">
+            <vk-nav-item 
+                v-for="(value,index) in this.links" 
+                :key="index" 
+                :title="value.title"
+                :href="value.to"
+            ></vk-nav-item>
+            <vk-nav-item href="#" title="Logout" @click.prevent="logout" v-if="$store.getters.isAuthenticated"></vk-nav-item>
+        </vk-nav>
+        </div>
+    </vk-modal-full>
 </div>
 </template>
 
@@ -52,7 +74,7 @@
                   type: Number,
                   value: 4
                 },
-                links: []
+                links: [],
             }
         },
         computed: {
@@ -75,6 +97,14 @@
             banner:{
               get(){
                 return this.$store.getters.banner_show;
+              },
+              set(val){
+                return val;
+              }
+            },
+            show:{
+              get(){
+                return this.$store.getters.sidebar;
               },
               set(val){
                 return val;
@@ -201,3 +231,17 @@
     }
 </script>
 
+<style lang="scss">
+  .uk-modal-full{
+
+      .uk-modal-dialog{
+        top:0 !important;
+        bottom: 0 !important;
+        position: fixed !important;
+        align-items: center !important;
+        justify-content: center;
+        display: flex;
+      }
+
+  }
+</style>
