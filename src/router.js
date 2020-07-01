79f723dab6079ad9115b19c9797b69cf50106d8a
Bralco Creations <info@bralcocreations.co.ke>
2020-07-01 15:36:39 +0300

    Router links update and htaccess update 0.9.10

--- src/router.js
+++ src/router.js
@@ -13,6 +13,7 @@ import HomeAbout from './components/home/layouts/AboutusComponent.vue';
 import HomeDocumentaries from './components/home/layouts/DocumentariesComponent.vue';
 import HomePlans from './components/home/layouts/PlansComponent.vue';
 import HomePlan from './components/home/layouts/PlanComponent.vue';
+import HomePlanItem from './components/home/layouts/show/PlanItemComponent.vue';
 import HomeAuth from './components/home/layouts/AuthComponent.vue';
 import HomeSignup from './components/home/layouts/SignupComponent.vue';
 import HomeProfile from './components/home/layouts/ProfileComponent.vue';
@@ -25,7 +26,7 @@ import NotFound from './components/global/NotFoundComponent.vue';
 Vue.use(Router);
 
 const router = new Router({ 
-   mode: "hash",
+   mode: "history",
    routes: [
             {
             path: '/',
@@ -35,14 +36,40 @@ const router = new Router({
                         path: '',
                         components: {
                             view: HomeIndex,
-                        }
+                        },
+                        meta: {
+                            title: 'Home - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          }
                     },
                     {
                         name:"about",
                         path: '/aboutus',
                         components: {
                             view: HomeAbout,
-                        }
+                        },
+                        meta: {
+                            title: 'About Us - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          }
                     },
                     {
                         name: "documentaries",
@@ -56,6 +83,19 @@ const router = new Router({
                             } else {
                                 next({name: "auth"});
                             }
+                        },
+                        meta: {
+                        title: 'Documentaries - 4IRCLUB',
+                        metaTags: [
+                            {
+                            name: 'description',
+                            content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                            },
+                            {
+                            property: 'og:description',
+                            content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                            }
+                        ]
                         }
                     },
                     {
@@ -70,21 +110,73 @@ const router = new Router({
                             } else {
                                 next({name: "auth"});
                             }
-                        }
+                        },
+                        meta: {
+                            title: 'Profile - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          }
                     },
                     {
                         name: "plans",
                         path: '/plans',
+                        components: {
+                            view: HomePlans,
+                        },
+                        meta: {
+                            title: 'Plans - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          }
+                    },
+                    {
+                        path: '/plan',
                         children: [
                             {
                                 name: "plan",
                                 path: ':item',
-                                component: HomePlan
+                                component: HomePlanItem
                             },
                         ],
                         components: {
-                            view: HomePlans,
-                        }
+                            view: HomePlan
+                        },
+                        beforeEnter (to,from, next) {
+                            if( to.params['item'] != undefined ){
+                                next();
+                            } else {
+                                next({name: "notfound"});
+                            }
+                        },
+                        meta: {
+                            title: 'Plan - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          }
                     },
                     {
                         name: "resources",
@@ -92,6 +184,19 @@ const router = new Router({
                         components: {
                             view: HomeResources,
                         },
+                        meta: {
+                            title: 'Resources - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          },
                         beforeEnter (to, from, next) {
                             if( stored.getters.isAuthenticated ){
                                 next();
@@ -106,6 +211,19 @@ const router = new Router({
                         components: {
                             view: HomeGlossary,
                         },
+                        meta: {
+                            title: 'Glossary - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          },
                         beforeEnter (to, from, next) {
                             if( stored.getters.isAuthenticated ){
                                 next();
@@ -120,6 +238,19 @@ const router = new Router({
                         components: {
                             view: HomeCheckout,
                         },
+                        meta: {
+                            title: 'Checkout - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Life Long Learning to re-engineer your career, institution and business to be adaptive to the demands of the emerging 4th Industrial Revolution.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Reskilling – the process of learning new skills so you can do a different job, or of training people to do a different job. ... While upskilling points out the same, it focuses more on improving worker's skills so they can work within the same job."
+                              }
+                            ]
+                          },
                         beforeEnter (to, from, next) {
                             if( stored.getters.isAuthenticated ){
                                 next();
@@ -134,6 +265,19 @@ const router = new Router({
                         components: {
                             view: HomeSignup,
                         },
+                        meta: {
+                            title: 'SignUp - 4IRCLUB',
+                            metaTags: [
+                                {
+                                    name: 'description',
+                                    content: "Create a free account to access the features in the platform"
+                                },
+                                {
+                                    property: 'og:description',
+                                    content: "Register requires personal details sent to the email address provided by the user."
+                                }
+                            ]
+                          },
                         beforeEnter (to,from, next) {
                             if( stored.state.app.auth.isAuthenticated ){
                               next({name: "home"});
@@ -147,7 +291,20 @@ const router = new Router({
                         path: '/email/verify/:token',
                         components: {
                             view: HomeVerify,
-                        }
+                        },
+                        meta: {
+                            title: 'Email Verify - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: "Verify if the email is authenticated & accepted"
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Activates the account made by the user. This allows users to be able to login to the platform and interact with the features."
+                              }
+                            ]
+                          }
                     },
                     {
                         name: "auth",
@@ -155,6 +312,19 @@ const router = new Router({
                         components: {
                             view: HomeAuth,
                         },
+                        meta: {
+                            title: 'SignIn - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: "Create a free account to access the features in the platform"
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Register requires personal details sent to the email address provided by the user."
+                              }
+                            ]
+                          },
                         beforeEnter (to,from, next) {
                             if( stored.state.app.auth.isAuthenticated ){
                               next({name: "home"});
@@ -168,7 +338,20 @@ const router = new Router({
                         path: '/404',
                         components: {
                             view: NotFound,
-                        }
+                        },
+                        meta: {
+                            title: 'Page Not Found - 4IRCLUB',
+                            metaTags: [
+                              {
+                                name: 'description',
+                                content: 'Resource could not be located.'
+                              },
+                              {
+                                property: 'og:description',
+                                content: "Fetching resource but nothing was found."
+                              }
+                            ]
+                          }
                     },
             ],
             components: {
@@ -181,6 +364,37 @@ const router = new Router({
 });
 
 router.beforeEach((to,from, ...next) => {
+    // This goes through the matched routes from last to first, finding the closest route with a title.
+    // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+    const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+    // Find the nearest route element with meta tags.
+    const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+    // const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+    // If a route with a title was found, set the document (page) title to that value.
+    if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+    // Remove any stale meta tags from the document using the key attribute we set below.
+    Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+    // Skip rendering meta tags if there are none.
+    if(!nearestWithMeta) return next();
+
+    // Turn the meta tag definitions into actual elements in the head.
+    nearestWithMeta.meta.metaTags.map(tagDef => {
+        const tag = document.createElement('meta');
+
+        Object.keys(tagDef).forEach(key => {
+        tag.setAttribute(key, tagDef[key]);
+        });
+
+        // We use this to track which meta tags we create, so we don't interfere with other ones.
+        tag.setAttribute('data-vue-router-controlled', '');
+        return tag;
+    })
+    // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag));
     stored.commit('loader',true);
     // console.log(next);
     next[0]();
