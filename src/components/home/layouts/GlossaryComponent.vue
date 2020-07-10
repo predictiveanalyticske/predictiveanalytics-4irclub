1695be75c399c2185954b01a9b686e013c4712fe
Bralco Creations <info@bralcocreations.co.ke>
2020-07-10 12:52:47 +0300

    Update glossary component 0.9.14

--- src/components/home/layouts/GlossaryComponent.vue
+++ src/components/home/layouts/GlossaryComponent.vue
@@ -1,9 +1,15 @@
 <template>
-    <vk-grid class="uk-child-width-1-1 uk-margin-remove" v-if="$route.params.item == undefined">
+    <vk-grid class="uk-child-width-1-1 uk-margin-remove">
         <div class="uk-padding-remove uk-margin-remove">
           <vk-card class="uk-padding-large">
-              <h3>Below is a list of resources to utilize and read through. </h3>
-              
+              <vk-grid class="uk-child-width-1-1 uk-margin-remove">
+                  <div>
+
+                  </div>
+                  <div>
+
+                  </div>
+              </vk-grid>
           </vk-card>
         </div>
     </vk-grid>
@@ -28,7 +34,7 @@
             checkSubscription(){
                 switch( JSON.parse(this.$store.getters.isSubscribed) && JSON.parse(this.$store.getters.isPaid) ){
                     case true:
-                        this.initData();
+                        this.iniData();
                     break;
                     case false:
                         this.$router.push({name:"home"});
@@ -36,18 +42,9 @@
                 }
             },
             iniData(){
-                this.bralcoaxios({ url: this.$store.state.app.env.backend_url + "/api/v1/4irclub/glossary", request: "GET" }).then( (response) => {
-                    var resolve = this.bralcoresponse(response);
-                    this.data   = resolve.data.resources;
-                    let items = [];
-                    for(let value of this.data){
-                        items.push([{
-                            active: true,
-                            title: value.name,
-                            html:  '<div class="uk-padding-small"><vk-icon icon="download"></vk-icon><a href="'+this.$store.getters.backendurl+'"/api/v1/resources/download/{item}"" target="_blank" class="uk-button uk-button-default">Download</a></div>'
-                        }]);
-                    }
-                    this.cards = items; 
+                this.bralcoaxios({ url: this.$store.state.app.env.backend_url + "/api/v1/glossary", request: "GET" }).then( (response) => {
+                    let resolve = this.bralcoresponse(response);
+                    console.log(resolve)
                 });
                 this.$store.commit('banner_title','Glossary')
                 this.$store.commit('banner_content','');
