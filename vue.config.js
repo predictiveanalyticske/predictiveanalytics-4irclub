329253b1a389fd252b3a9e449a8993f7a0609007
Bralco Creations <info@bralcocreations.co.ke>
2020-07-22 12:57:05 +0300

    Fix quoted scrip files

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 // vue.config.js
 module.exports = {
+chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].minify.removeAttributeQuotes = false;
+            return args;
+        })
+},
 configureWebpack: {
   output: {
     publicPath: '/', // if you don't put the "/" here, you get this error:
   },  
-}
+},
 }
