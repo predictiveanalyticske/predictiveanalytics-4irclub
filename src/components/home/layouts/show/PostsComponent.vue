67455bce098d9a9ecf83dbf13f0020b7f84fe8da
Bryant Rotich <bryant@bralcocreations.co.ke>
2020-11-11 07:55:56 +0300

    Post update

--- src/components/home/layouts/show/PostsComponent.vue
+++ src/components/home/layouts/show/PostsComponent.vue
@@ -2,8 +2,12 @@
     <vk-grid class="uk-child-width-1-1 uk-width-2-3@l uk-width-2-3@m">
         <div v-for="(value, index) in data" :key="index">
             <vk-card type="default">
+                 <vk-label slot="badge">{{ value.type }}</vk-label>
                 <div slot="media-top">
-                    <iframe class="uk-width-1-1" height="500" :src="value.link" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                    <iframe v-if="value.type == 'youtube'" class="uk-width-1-1" height="500" :src="value.link" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                    <div v-else class="uk-text-center">
+                        <vk-icon-link :href="value.link" @click.prevent="playVideo" ratio="10" icon="play"></vk-icon-link>
+                    </div>
                 </div>
                 <div>
                     <h3 class="uk-text-warning">{{ value.title }}</h3>
@@ -17,6 +21,11 @@
 <script>
     export default {
         props:["data"],
+        methods: {
+            playVideo(){
+                window.open(event.currentTarget.attributes.href.value,"_blank");
+            }
+        },  
         name: "posts",
     }
 </script>
